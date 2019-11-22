42dad4f6d82a9b8a84586f5b356f8061cfe016a4
kristofdewin <kristofdewin1982@gmail.com>
2019-11-22 14:17:40 +0100

    first commit

---
+++ src/main/java/guru/springframework/sfppetclinic/SfpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfpPetClinicApplication.class, args);
+    }
+
+}
