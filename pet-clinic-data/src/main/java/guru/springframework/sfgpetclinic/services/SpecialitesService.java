3d4b0117edd7d15d07237c99edeb43f74ee8487e
kristofdewin <kristofdewin1982@gmail.com>
2020-02-06 13:18:28 +0100

    added SpecialitesService and SpecialityMapService closes #32

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialitesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialitesService extends CrudService<Specialty,Long> {
+}
