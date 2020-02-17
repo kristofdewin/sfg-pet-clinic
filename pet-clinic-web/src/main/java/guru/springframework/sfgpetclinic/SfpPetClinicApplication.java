bdd11add9509ab05a9a3d2135c08c59510ceaa26
kristofdewin <kristofdewin1982@gmail.com>
2020-02-17 10:21:06 +0100

    added spring security and security config to pom, excluded ErrorMvcAutoConfiguration.class from springbootapplication due clash with vaadin

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfpPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfpPetClinicApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+//exclude due to bug in vaadin
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class SfpPetClinicApplication {
 
     public static void main(String[] args) {
